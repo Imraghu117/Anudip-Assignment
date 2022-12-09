4b0e192d7e155c6c1bb6de54c04550fb767d659b
Imraghu117 <87611485+Imraghu117@users.noreply.github.com>
2022-12-09 17:35:11 +0530

    Update OddNumbers.java

--- projectlabtest1/src/projectlabtest1/OddNumbers.java
+++ projectlabtest1/src/projectlabtest1/OddNumbers.java
@@ -11,6 +11,7 @@ public class OddNumbers {
 
 	            int number = 20;
 	            System.out.println("List of odd numbers from 1 to 20 ::");
+			//logic for Odd Numbers Between 1 to 20.
 	            for (int i = 1; i <= number; i++) {
 
 	                if (i % 2 != 0) {
