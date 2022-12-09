4322b7915e937296e4c472cfc2e173b4567fbdce
Imraghu117 <87611485+Imraghu117@users.noreply.github.com>
2022-12-09 17:30:59 +0530

    Add files via upload

---
+++ projectlabtest1/src/projectlabtest1/OddNumbers.java
@@ -0,0 +1,28 @@
+package projectlabtest1;
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		OddNumbers();
+		
+	}
+	  public static void OddNumbers() {
+	        {
+
+	            int number = 20;
+	            System.out.println("List of odd numbers from 1 to 20 ::");
+	            for (int i = 1; i <= number; i++) {
+
+	                if (i % 2 != 0) {
+	                    System.out.print(i + " ");
+	                }
+	            }
+
+	        }
+	  }
+	 
+}
+	    
+	  
+	
+
